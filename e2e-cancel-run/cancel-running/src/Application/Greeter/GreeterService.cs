bdc7959131451e7c92a1c2d35f68b79fc2d83c18
TemplateService <template-service@garmin.com>
2026-03-28 23:26:40 +0800

    [IDP]Initial commit from template

---
+++ e2e-cancel-run/cancel-running/src/Application/Greeter/GreeterService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Greeter;
+
+public class GreeterService : IGreeterService
+{
+    public string SayHello(string name)
+        => $"Hello {name} (from Application Layer)";
+}
